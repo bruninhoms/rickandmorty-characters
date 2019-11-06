6ae783922cbc08aa182bb0ad9604f812220f395e
Bruno Mattias <brunodemattias@gmail.com>
2019-11-06 18:32:02 -0300

    Updates

---
+++ precache-manifest.ae3f7b7a2fd8e2b76ce8ed4b4a609717.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebdf7144e165fb936c32517e57b6b3ff",
+    "url": "/rickandmorty-characters/index.html"
+  },
+  {
+    "revision": "e5a0cf35bcedf8234c5e",
+    "url": "/rickandmorty-characters/static/css/main.8979eea7.chunk.css"
+  },
+  {
+    "revision": "37e36838c72029ca4c40",
+    "url": "/rickandmorty-characters/static/js/2.6273cec0.chunk.js"
+  },
+  {
+    "revision": "e5a0cf35bcedf8234c5e",
+    "url": "/rickandmorty-characters/static/js/main.638cae87.chunk.js"
+  },
+  {
+    "revision": "4ceaa4982fd6a6723b99",
+    "url": "/rickandmorty-characters/static/js/runtime-main.14e05224.js"
+  }
+]);
